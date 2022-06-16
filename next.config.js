e20e12cd0abc8716a6b21660ccaa4f0b0068c8a1
SJ Triffon <who@sjcoded.com>
2022-06-16 13:48:27 +1000

    update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+module.exports = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, "styles")],
 	},
 };
-
-module.exports = nextConfig;
